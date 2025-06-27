06971fb6363033499ffc2170c2f40ce076821a96
will <fournier.wilf@gmail.com>
2025-06-27 20:54:10 +0000

    creating dynamic menu

---
+++ src/app/nav-bar/nav-bar.ts
@@ -0,0 +1,32 @@
+import { Component, ElementRef, OnInit, viewChild } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [RouterLink],
+  templateUrl: './nav-bar.html',
+  styleUrl: './nav-bar.css'
+})
+export class NavBar {
+
+  sideNav = viewChild<ElementRef<HTMLElement>>('sidenav');
+
+  openSideMenu() {
+    const elementSideNav = this.sideNav();
+    if (!elementSideNav)
+      return;
+
+      elementSideNav.nativeElement.style.width = "250px";
+
+  }
+
+
+  closeSideMenu() {
+    const elementSideNav = this.sideNav();
+    if (!elementSideNav)
+      return;
+
+      elementSideNav.nativeElement.style.width = "0px";
+
+  }
+}
