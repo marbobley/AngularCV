d3bac289dbdab3726760bff9ac5403a28e63e4b0
will <fournier.wilf@gmail.com>
2025-06-28 08:34:23 +0000

    Add some routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { About } from './about/about';
 
 export const routes: Routes = [
+    { path: '**', redirectTo: 'home', pathMatch: 'full' },
     {path: 'about', component:About,  }
 ];
