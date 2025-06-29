f2e5a1f62dc126950b3a859593fe509083c99f76
will <fournier.wilf@gmail.com>
2025-06-29 10:18:12 +0000

    add experience page add skill page bind route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { About } from './about/about';
 import { Home } from './home/home';
+import { Experience } from './experience/experience';
+import { Skill } from './skill/skill';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home' , component:Home },
-    {path: 'about', component:About,  }
+    {path: 'about', component:About,  },
+    {path: 'experience', component:Experience,  },
+    {path: 'skill', component:Skill,  }
 ];
