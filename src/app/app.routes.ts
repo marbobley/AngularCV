d9cfe66edc7fe5654518a266b98b646fbab7fe7a
will <fournier.wilf@gmail.com>
2025-06-29 09:57:15 +0000

    add home page Update nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { About } from './about/about';
+import { Home } from './home/home';
 
 export const routes: Routes = [
-    { path: '**', redirectTo: 'home', pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home' , component:Home },
     {path: 'about', component:About,  }
 ];
