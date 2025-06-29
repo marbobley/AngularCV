d3330519134236f5beb2a30f07d552d5b6ad4343
will <fournier.wilf@gmail.com>
2025-06-29 10:45:28 +0000

    add link to play ground

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { About } from './about/about';
 import { Home } from './home/home';
 import { Experience } from './experience/experience';
 import { Skill } from './skill/skill';
+import { Playground } from './playground/playground';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home' , component:Home },
     {path: 'about', component:About,  },
     {path: 'experience', component:Experience,  },
-    {path: 'skill', component:Skill,  }
+    {path: 'skill', component:Skill,  },
+    {path: 'playground', component:Playground,  }
 ];
