8427c42c8e52e7370604c034d40ee48ee9c31907
will <fournier.wilf@gmail.com>
2025-06-29 13:39:19 +0000

    playing with responsivness

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,14 +1,22 @@
-import { Component, ElementRef, OnInit, viewChild } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, viewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LayoutModule, BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css'
 })
 export class NavBar {
 
+  private breakpointObserver = inject(BreakpointObserver);
+  
+  isDesktop$ = this.breakpointObserver
+    .observe([Breakpoints.Tablet, Breakpoints.Large, Breakpoints.XLarge])
+    .pipe(map(result => result.matches));
   sideNav = viewChild<ElementRef<HTMLElement>>('sidenav');
 
   openSideMenu() {
