90f7c914b91b7c8197bdb5538b2d3b06ac52bd5f
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-06-30 16:28:35 +0200

    qdd some technical content

--- src/app/card-technical/card-technical.ts
+++ src/app/card-technical/card-technical.ts
@@ -1,17 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
+
 
 @Component({
   selector: 'app-card-technical',
-  imports: [MatButtonModule, MatCardModule],
+  imports: [MatButtonModule, MatCardModule,MatProgressBarModule],
   templateUrl: './card-technical.html',
   styleUrl: './card-technical.css'
 })
 export class CardTechnical {
   @Input() langage = 'C#';
+  @Input() evalution = 'Intermediate';
   @Input() level = 9; 
-  @Input() yearOfExperience = 5;
+  @Input() yearOfExperience = '5';
   @Input() description='Pratiquer dans un contexte TMA avec beaucoup de correctifs ';
 
 }
