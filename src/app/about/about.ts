b2725331b5bf5128e6b161d4959cfc204ae9b00c
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-06-30 15:12:37 +0200

    add content to about

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
-export class About {
+export class About implements OnInit{
+  isPhonePortrait = false;
+
+  constructor(private responsive: BreakpointObserver)
+  {
+
+  }
+
+  ngOnInit(): void {
+    this.responsive.observe(Breakpoints.HandsetPortrait)
+    .subscribe(result => {
+      this.isPhonePortrait = false;
+
+      if(result.matches)
+      {
+        this.isPhonePortrait = true;
+      }
+    })
+  }
 
 }
