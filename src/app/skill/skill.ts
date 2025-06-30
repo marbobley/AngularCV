ed13a24a4e9c8afaac6bad59ae8b1f7e855889d2
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-06-30 15:27:59 +0200

    init skils

--- src/app/skill/skill.ts
+++ src/app/skill/skill.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { CardQuality } from '../card-quality/card-quality';
 
 @Component({
   selector: 'app-skill',
-  imports: [],
+  imports: [NgClass,MatTabsModule, CardQuality],
   templateUrl: './skill.html',
-  styleUrl: './skill.css'
+  styleUrl: './skill.css',
 })
-export class Skill {
+export class Skill implements OnInit {
+  isPhonePortrait = false;
 
+  constructor(private responsive: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.responsive.observe(Breakpoints.HandsetPortrait).subscribe((result) => {
+      this.isPhonePortrait = false;
+
+      if (result.matches) {
+        this.isPhonePortrait = true;
+      }
+    });
+  }
 }
