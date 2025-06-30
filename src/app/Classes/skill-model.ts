15f2a0db35bb79525d8800a057ee5d165313a48f
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-06-30 16:33:06 +0200

    add skill class

---
+++ src/app/Classes/skill-model.ts
@@ -0,0 +1,6 @@
+export class SkillModel {
+
+    constructor(private _langage: string, private _level: string , private _yearOfExperence: string, private _description: string, private _evaluation: string){
+
+    }    
+}
