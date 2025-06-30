bbec34735b558170fd67e42e48c57ea7fec3b457
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-06-30 14:50:47 +0200

    center card

--- src/app/card-quality/card-quality.ts
+++ src/app/card-quality/card-quality.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { Component, Input, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-card-quality',
-  imports: [MatButtonModule,MatCardModule],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './card-quality.html',
-  styleUrl: './card-quality.css'
+  styleUrl: './card-quality.css',
 })
 export class CardQuality {
-  title: string = 'Autonomie';
-  subtitle: string = 'Indépendant, mais présent';
-  content1: string = "J’ai souvent travaillé sur des projets de petites tailles : Satisfait des résultats et d’un compte rendu d’activité régulier, mes responsables m’ont laissé gérer le projet de façon autonome.";
-  content2: string = "Cette autonomie m’a permis de monter en compétences sur la gestion du temps, la planification et le relationnelle client.";
-
+  @Input() title: string = '';
+  @Input() subtitle: string = '';
+  @Input() content1: string =
+    'J’ai souvent travaillé sur des projets de petites tailles : Satisfait des résultats et d’un compte rendu d’activité régulier, mes responsables m’ont laissé gérer le projet de façon autonome.';
+  @Input() content2: string =
+    'Cette autonomie m’a permis de monter en compétences sur la gestion du temps, la planification et le relationnelle client.';
 }
