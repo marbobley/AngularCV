c417e3546b6780454c040ed629c6f7eff2496d83
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-06-30 09:48:06 +0200

    Add some responsive case to try it

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [MatGridListModule,RouterLink],
+  imports: [MatGridListModule,RouterLink, NgClass],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit {
+
+  isPhonePortrait = false;
+
+  constructor(private responsive: BreakpointObserver)
+  {
+
+  }
+
+  ngOnInit(): void {
+    this.responsive.observe(Breakpoints.HandsetPortrait)
+    .subscribe(result => {
+      this.isPhonePortrait = false;
+
+      if(result.matches)
+      {
+        this.isPhonePortrait = true;
+        console.log('HANDSETPORTRAIT');
+      }
+    })
+  }
 
 }
