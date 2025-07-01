9042259a084ca37fbaed8f0f032bdbdf2959a7f3
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-07-01 15:55:08 +0200

    starting working timeline

--- src/app/experience/experience.ts
+++ src/app/experience/experience.ts
@@ -1,11 +1,13 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './experience.html',
   styleUrl: './experience.css'
 })
 export class Experience {
+  isPhonePortrait = false;
 
 }
