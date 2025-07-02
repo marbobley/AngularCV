30a460292d49ba68f91e51954e2f2a356c5cfb08
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-07-02 10:49:40 +0200

    passing directly object to component

--- src/app/card-skill/card-skill.ts
+++ src/app/card-skill/card-skill.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { SkillModel } from '../Classes/skill-model';
 
 @Component({
   selector: 'app-card-skill',
@@ -16,12 +17,5 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   styleUrl: './card-skill.css',
 })
 export class CardSkill {
-  @Input() typeSkill = 'Langage';
-  @Input() langage = 'C#';
-  @Input() evalution = 'Intermediate';
-  @Input() level = 9;
-  @Input() yearOfExperience = '5';
-  @Input() description =
-    'Pratiquer dans un contexte TMA avec beaucoup de correctifs ';
-  @Input() imgSrc = '';
+  @Input() skill!: SkillModel;
 }
