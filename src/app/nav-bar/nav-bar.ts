d22e9e358707c2849bc172b71b057fd1bdce1e17
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-07-03 10:13:50 +0200

    cleaning navbar

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,40 +1,27 @@
-import { Component, ElementRef, inject, OnInit, viewChild } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { LayoutModule, BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { map } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   imports: [RouterLink, AsyncPipe],
   templateUrl: './nav-bar.html',
-  styleUrl: './nav-bar.css'
+  styleUrl: './nav-bar.css',
 })
 export class NavBar {
-
-  private breakpointObserver = inject(BreakpointObserver);
-  
-  isDesktop$ = this.breakpointObserver
-    .observe([Breakpoints.Tablet, Breakpoints.Large, Breakpoints.XLarge])
-    .pipe(map(result => result.matches));
   sideNav = viewChild<ElementRef<HTMLElement>>('sidenav');
 
   openSideMenu() {
     const elementSideNav = this.sideNav();
-    if (!elementSideNav)
-      return;
+    if (!elementSideNav) return;
 
-    elementSideNav.nativeElement.style.width = "250px";
-
+    elementSideNav.nativeElement.style.width = '250px';
   }
-
 
   closeSideMenu() {
     const elementSideNav = this.sideNav();
-    if (!elementSideNav)
-      return;
+    if (!elementSideNav) return;
 
-    elementSideNav.nativeElement.style.width = "0px";
-
+    elementSideNav.nativeElement.style.width = '0px';
   }
 }
