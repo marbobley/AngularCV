2dfcbd7298501d7ea6ea66dd61a1711b2d1ee713
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-07-03 09:38:52 +0200

    adjusting css on experience page

--- src/app/experience/experience.ts
+++ src/app/experience/experience.ts
@@ -1,13 +1,37 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { NgClass } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  viewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-experience',
   imports: [NgClass],
   templateUrl: './experience.html',
-  styleUrl: './experience.css'
+  styleUrl: './experience.css',
 })
-export class Experience {
+export class Experience implements OnInit  {
   isPhonePortrait = false;
+
+  constructor(private responsive: BreakpointObserver)
+  {
+
+  }
+
+  ngOnInit(): void {
+    this.responsive.observe(Breakpoints.HandsetPortrait)
+    .subscribe(result => {
+      this.isPhonePortrait = false;
+
+      if(result.matches)
+      {
+        this.isPhonePortrait = true;
+      }
+    })
+  }
 
 }
