c6d7dcb77c06d86586d0b8943875946c8da996f7
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-07-03 10:10:47 +0200

    update about with new layout service

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,31 +1,15 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { NgClass } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { LayoutService } from '../Services/layout-service';
 
 @Component({
   selector: 'app-about',
   imports: [NgClass],
   templateUrl: './about.html',
-  styleUrl: './about.css'
+  styleUrl: './about.css',
 })
-export class About implements OnInit{
-  isPhonePortrait = false;
-
-  constructor(private responsive: BreakpointObserver)
-  {
-
-  }
-
-  ngOnInit(): void {
-    this.responsive.observe(Breakpoints.HandsetPortrait)
-    .subscribe(result => {
-      this.isPhonePortrait = false;
-
-      if(result.matches)
-      {
-        this.isPhonePortrait = true;
-      }
-    })
-  }
-
+export class About {
+  private layoutService = inject(LayoutService);
+  isPhonePortrait = this.layoutService.isPhonePortrait;
 }
