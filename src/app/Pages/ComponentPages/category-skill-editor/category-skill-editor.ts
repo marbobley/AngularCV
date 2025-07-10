630940e53e95a8f8c6c383f3b0a6b8af8c97a019
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-07-10 15:18:14 +0200

    add post control for category skill

--- src/app/Pages/ComponentPages/category-skill-editor/category-skill-editor.ts
+++ src/app/Pages/ComponentPages/category-skill-editor/category-skill-editor.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CategorySkillInterface } from '../../../Interface/CategorySkillInterface';
 
 @Component({
   selector: 'app-category-skill-editor',
   imports: [ReactiveFormsModule],
   templateUrl: './category-skill-editor.html',
-  styleUrl: './category-skill-editor.css'
+  styleUrl: './category-skill-editor.css',
 })
 export class CategorySkillEditor {
-  categorySkillForm = new FormControl('');
+  categorySkill = output<CategorySkillInterface>();
 
+  name = new FormControl('');
+  description = new FormControl('');
+
+  sendCategorySkill() {
+    const curName = this.name.value;
+    const curDesc = this.description.value;
+
+    if (curName && curDesc) {
+      const currentCategorySkill: CategorySkillInterface = {
+        name: curName,
+        description: curDesc,
+      };
+
+      this.categorySkill.emit(currentCategorySkill);
+    }
+  }
 }
--- src/app/Pages/admin/admin.ts
+++ src/app/Pages/admin/admin.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CategorySkillList } from '../ComponentPages/category-skill-list/category-skill-list';
-import { CategorySkillEditor } from "../ComponentPages/category-skill-editor/category-skill-editor";
+import { CategorySkillEditor } from '../ComponentPages/category-skill-editor/category-skill-editor';
+import { CategorySkillInterface } from '../../Interface/CategorySkillInterface';
+import { CategorySkillApiService } from '../../Services/api/category-skill-api-service';
 
 @Component({
   selector: 'app-admin',
@@ -9,4 +11,14 @@ import { CategorySkillEditor } from "../ComponentPages/category-skill-editor/cat
   styleUrl: './admin.css',
 })
 export class Admin {
+
+  private categorySkillApi = inject(CategorySkillApiService);
+
+  categorySkill: CategorySkillInterface | null = null;
+
+  categorySkillEmit($event: CategorySkillInterface) {
+
+    this.categorySkillApi.postCategorySkill($event).subscribe( res => console.log(res));
+    
+  }
 }
