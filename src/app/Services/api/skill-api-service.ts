b3d078b0cb883027882147ab7be93a6df2a8f57f
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-07-11 14:12:02 +0200

    add post put and delete skill api

--- src/app/Services/api/skill-api-service.ts
+++ src/app/Services/api/skill-api-service.ts
@@ -1,14 +1,16 @@
 import { inject, Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment.development';
 import { SkillInterface } from '../../Interface/SkillInterface';
+import { TokenService } from './token-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SkillApiService {
   private http = inject(HttpClient);
+    private token = inject(TokenService);
   urlApiSkill = `${environment.apiURL}/skills`;
 
   getSkill(id: number): Observable<SkillInterface> {
@@ -22,4 +24,38 @@ export class SkillApiService {
       .get<SkillInterface[]>(this.urlApiSkill)
       .pipe(tap());
   }
+  
+    postSkill(
+      categorySkill: SkillInterface
+    ): Observable<SkillInterface> {
+      const headers = new HttpHeaders().set('Authorization', this.token.getAuthenticateToken());
+      return this.http
+        .post<SkillInterface>(this.urlApiSkill, categorySkill, {
+          headers: headers,
+        })
+        .pipe();
+    }
+  
+    putSkill(
+      skill: SkillInterface
+    ): Observable<SkillInterface> {
+      const headers = new HttpHeaders().set('Authorization', this.token.getAuthenticateToken());
+      return this.http
+        .put<SkillInterface>(
+          this.urlApiSkill + '/' + skill.id,
+          skill, {
+          headers: headers,
+        }
+        )
+        .pipe();
+    }
+  
+    deleteSkill(id: number): Observable<SkillInterface> {
+      const headers = new HttpHeaders().set('Authorization', this.token.getAuthenticateToken());
+      return this.http
+        .delete<SkillInterface>(this.urlApiSkill + '/' + id, {
+          headers: headers,
+        })
+        .pipe();
+    }
 }
